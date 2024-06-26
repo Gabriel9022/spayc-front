7fbc4d1f0ac9cc1cbb9fc591eba23670ea003cda
Gabriel <gabi300709@gmail.com>
2024-06-26 18:23:57 -0300

    Router installed and tested

--- spayc-frontend/src/App.tsx
+++ spayc-frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
 
