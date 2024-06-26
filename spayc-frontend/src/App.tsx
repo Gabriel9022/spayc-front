3ad0ae23f0cdad0d949efa852ac63b336a86831a
Gabriel <gabi300709@gmail.com>
2024-06-26 18:58:49 -0300

    routing check and tested

--- spayc-frontend/src/App.tsx
+++ spayc-frontend/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Services from './Components/Services/Services';
+import CareOptions from './Components/CareOptions/CareOptions';
+import InstitutionalServices from './Components/InstitutionalServices/InstitutionalServices';
+import WorkWithUs from './Components/WorkWithUs/WorkWithUs';
+import Contact from './Components/Contact/Contact';
+import AboutUs from './Components/AboutUs/AboutUs';
+import Loggin from './Components/Loggin/Loggin';
+import EditPanel from './Components/EditPanel/EditPanel';
 import './App.css'
 
 const App: React.FC = () => {
@@ -9,6 +17,14 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/servicios" element={<Services/>}/>
+        <Route path="/opciones-de-cuidados" element={<CareOptions/>}/>
+        <Route path="/corporativo" element={<InstitutionalServices/>}/>
+        <Route path="/trabaja-con-nosotros" element={<WorkWithUs/>}/>
+        <Route path="/contactanos" element={<Contact/>}/>
+        <Route path="/nosotros" element={<AboutUs/>}/>
+        <Route path="/loggin" element={<Loggin/>}/>
+        <Route path="/panel" element={<EditPanel/>}/>        
       </Routes>
     </Router>
   )
