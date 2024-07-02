9e0b7c74e8ff8f3c3a765c6cc758b50f797fdd3c
Gabriel <gabi300709@gmail.com>
2024-07-02 01:14:22 -0300

    AboutUs static page

--- spayc-frontend/src/Components/AboutUs/AboutUs.tsx
+++ spayc-frontend/src/Components/AboutUs/AboutUs.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './AboutUs.css';
 
 const AboutUs: React.FC = () => {
   return (
-    <div>AboutUs</div>
+    <div className='AboutUs_component'>
+      <p className='AboutUs_p'>
+      Somos un equipo multidisciplinario, liderado por Licenciadas en Trabajo Social, con formación y experiencia en el trabajo con personas mayores.<br/> 
+      Porque no hay nada mejor que envejecer en casa, en un entorno hogareño, confortable, accesible, significativo y seguro.<br/>
+      SPAYC te ofrece personal altamente capacitado para brindar a las personas mayores y su familia, un servicio de apoyos y cuidados para el desarrollo de las actividades básicas, instrumentales y/o avanzadas de la vida diaria. Siempre identificando y respetando necesidades, gustos, deseos y preferencias de la persona mayor.<br/>
+      Nos caracterizamos por trabajar desde un enfoque de derechos, con perspectiva de género y mirada gerontológica. Nuestros profesionales se distinguen por realizar sus intervenciones desde el modelo de atención centrada en la persona, un “modelo de atención personalizado y profesionalizado que busca mejorar la calidad de vida de las personas mayores que precisan cuidados, dando máxima importancia al respeto a su individualidad, a su modo de vida y a sus decisiones o preferencias en el día a día”.
+      </p>
+    </div>
   )
 }
 
