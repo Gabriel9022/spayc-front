ceb00dc2810a22203dd8c68247eb3e10b1de4095
Gabriel <gabi300709@gmail.com>
2024-07-16 19:40:09 -0300

    some style for workWithUs page

--- spayc-frontend/src/Components/WorkWithUs/WorkWithUs.tsx
+++ spayc-frontend/src/Components/WorkWithUs/WorkWithUs.tsx
@@ -1,9 +1,172 @@
-import React from 'react'
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import WorkWithUsImage from '../../assets/images/WorkWithUs/WorkWithUs_img.png';
+import './WorkWithUs.css'
+
+type WorkWithUs_Inputs = {
+  name: string;
+  email: string;
+  message: string;
+  tel: string;
+  file: FileList;
+};
+
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
 
 const WorkWithUs: React.FC = () => {
+
+  const { 
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors } = useForm<WorkWithUs_Inputs>({
+    defaultValues: {
+      name: '',
+      email: '',
+      tel: '',
+      message: ''
+    }
+  });
+
+  const validateFileSize = (file: FileList | null): boolean => {
+    if (file && file[0] && file[0].size > MAX_FILE_SIZE) {
+      setError("file", {
+        type: "manual",
+        message: "El archivo excede el tamaño máximo de 2MB",
+      });
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmit: SubmitHandler<WorkWithUs_Inputs> = async (data) => {
+
+    if (!validateFileSize(data.file)) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('tel', data.tel);
+    formData.append('message', data.message);
+    formData.append('file', data.file[0]);
+
+    console.log("Esto es data: ", data)
+
+    try {
+      const response = await fetch('http://localhost:3001/workWithUs/workWithUs-contact-email', {
+        method: 'POST',
+        body: formData,
+      });
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div>WorkWithUs</div>
-  )
-}
+    <div className='WorkWithUs_component'>
+      <div className='WorkWithUs_sections'>
+        <div className='WorkWithUs_section_info'>
+          <div className='WorkWithUs_info_container'>
+            <div className='WorkWithUs_info'>
+              <h1 className='WorkWithUs_info_title'>Únete a SPAYC: Trabaja con Nosotros</h1>
+              <p className='WorkWithUs_info_description'>En SPAYC, Servicios Profesionales de Apoyos y Cuidados para Personas Mayores, contamos con una sólida trayectoria en el cuidado y acompañamiento de personas mayores. Nos dedicamos a ofrecer servicios de la más alta calidad, enfocados en mejorar la calidad de vida de nuestros usuarios y sus familias.</p>
+              <p className='WorkWithUs_info_description'>Buscamos profesionales comprometidos y altamente capacitados que compartan nuestra pasión por el cuidado y el bienestar de las personas mayores. Si tienes experiencia en el rubro y un fuerte deseo de hacer una diferencia en la vida de nuestros clientes, ¡queremos conocerte! Únete a nuestro equipo y sé parte de una organización que valora la dedicación, el profesionalismo y el respeto.</p>
+            </div>
+            <div className='WorkWithUs_image_container'>
+              <img className='WorkWithUs_image' src={WorkWithUsImage} alt="imagen trabaja con nosotros" />
+            </div>
+          </div>
+        </div>
+        <div className='WorkWithUs_section_form'>
+          <div className='WorkWithUs_section_form_container'>
+            <p className='WorkWithUs_form_title'>Si querés trabajar con nosotros completa el siguiente formulario</p>
+            <div className='WorkWithUs_form_container'>
+              <form className='WorkWithUs_form' onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+                <div className='WorkWithUs_container_name'>
+                  <label>Nombre</label>
+                  <input className='WorkWithUs_input' placeholder='Nombre'
+                    {...register('name', {
+                      required: 'Campo requerido',
+                      pattern: {
+                        value: /^[A-Za-z\s]+$/,
+                        message: 'Solo letras y espacios están permitidos'
+                      }
+                    })}
+                  />
+                  {errors.name && <p>{errors.name.message}</p>}
+                </div>
+                <div className='WorkWithUs_container_email'>
+                  <label>Email</label>
+                  <input className='WorkWithUs_input'
+                    type="email" placeholder='Email'
+                    {...register('email', {
+                      required: 'Campo requerido.',
+                      pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: 'El formato del correo es inválido'
+                      }
+                    })}
+                  />
+                  {errors.email && <p className='Contact_errors'>{errors.email.message}</p>}
+                </div>
+                <div className='WorkWithUs_container_tel'>
+                  <label>Teléfono</label>
+                  <input className='WorkWithUs_input' type="tel" placeholder='Teléfono' {...register("tel",
+                    {
+                      required: "Este campo es obligatorio",
+                      minLength: {
+                        value: 6,
+                        message: "Cantidad de dígitos inválida"
+                      },
+                      maxLength: {
+                        value: 15,
+                        message: "Cantidad de dígitos inválida"
+                      },
+                      pattern: {
+                        value: /^[0-9()+-]+$/,
+                        message: 'Número de teléfono inválido.'
+                      }
+
+                    }
+                  )} />
+                  {errors.tel && <p className='Contact_errors'>{errors.tel?.message}</p>}
+                </div>
+                <div className='WorkWithUs_container_message'>
+                  <label>Mensaje</label>
+                  <textarea className='WorkWithUs_input' placeholder='Escriba aquí un mensaje'
+                    {...register('message', {
+                      required: 'Campo requerido.'
+                    })}
+                  />
+                  {errors.message && <p className='Contact_errors'>{errors.message.message}</p>}
+                </div>
+                <div className='WorkWithUs_container_file'>
+                  <label>Adjuntar (CV)</label>
+                  <input className='WorkWithUs_input_file'
+                    type="file"
+                    {...register('file', {
+                      required: 'El archivo de CV es requerido.'
+                    })}
+                    onChange={(e) => {
+                      clearErrors("file");
+                      validateFileSize(e?.target.files);
+                    }}
+                  />
+                  {errors.file && <p className='Contact_errors'>{errors.file.message}</p>}
+                </div>
+                <button className='WorkWithUs_submit' type="submit">Enviar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default WorkWithUs
