c240486a81f14ee06fa44a3e391251fc9a35181c
Gabriel <gabi300709@gmail.com>
2024-07-30 02:36:01 -0300

    some changes in navBar, links, routes, descriptions

--- spayc-frontend/src/Components/CareOptions/CareOptions.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CareOptions: React.FC = () => {
-  return (
-    <div>CareOptions</div>
-  )
-}
-
-export default CareOptions
--- spayc-frontend/src/Components/Contact/Contact.tsx
+++ spayc-frontend/src/Components/Contact/Contact.tsx
@@ -33,7 +33,7 @@ const Contact: React.FC = () => {
       <div className='Contact_sections'>
         <div className='Contact_intro_section'>
           <p className='Contact_intro_title'>¡Ponte en <h1>Contacto</h1> con Nosotros!</p>
-          <p className='Contact_intro_description'>En SPAYC, valoramos la comunicación directa y efectiva con nuestros clientes. Si tienes alguna pregunta, comentario o necesitas más información sobre nuestros servicios, no dudes en contactarnos. Estamos aquí para ayudarte.</p>
+          <p className='Contact_intro_description'>En SPAYC valoramos la comunicación directa y efectiva, si tiene alguna consulta o necesita más información sobre nuestros servicios, no dude en contactarnos y un asesor se comunicará con Ud.</p>
         </div>
         <div className='Contact_info_form_sections'>
           <div className='Contact_section_info'>
