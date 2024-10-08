c3afca0560e604de0270f4a7124065a5d1076098
Gabriel <gabi300709@gmail.com>
2024-10-08 20:45:46 -0300

    feat: apply client-requested updates - updated styles, modified footer, changed contact phone, added WhatsApp link, adjusted header

--- spayc-frontend/src/Components/Footer/Footer.tsx
+++ spayc-frontend/src/Components/Footer/Footer.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Footer.css';
 import { Link, useLocation } from 'react-router-dom';
-import facebook_black_icon from '../../assets/images/Footer/social_media_icons/facebook_black_icon.png';
 import facebook_circle_black_icon from '../../assets/images/Footer/social_media_icons/facebook_circle_black_icon.png';
 import instagram_black_icon from '../../assets/images/Footer/social_media_icons/instagram_black_icon.png';
-import instagram_circle_black_icon from '../../assets/images/Footer/social_media_icons/instagram_circle_black_icon.png';
 import logo from '../../assets/images/Footer/spayc_logo.svg';
 
 const Footer: React.FC = () => {
@@ -16,41 +14,32 @@ const Footer: React.FC = () => {
         <div className='Footer_section'>
             <div className='Footer_firstSection_container'>
                 <div className='logo_container'>
-                    <Link to='/'>
+                    <Link to='/inicio'>
                         <img src={logo}  alt="Logo SPAYC" className='logo' />
                         <p>Servicios Profesionales de Apoyo y Cuidado para Personas Mayores</p>
                     </Link>
-                </div>
-                <div className='socialMedia_container'>
-                    <div className='socialMedia_icon_container'>
-                        <a className='Footer_socialmedia_link' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                            <img className='socialMedia_icon' src={facebook_black_icon} alt="" />                           
-                        </a>
-                    </div>
-                    <div className='socialMedia_icon_container'>
-                        <a className='Footer_socialmedia_link' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                            <img className='socialMedia_icon' src={facebook_circle_black_icon} alt="" />
-                        </a>
-                    </div>
-                    <div className='socialMedia_icon_container'>
-                        <a className='Footer_socialmedia_link' href='https://www.instagram.com/spaycserviciospersonasmayores/' target="_blank" rel="noopener noreferrer">
-                            <img className='socialMedia_icon' src={instagram_black_icon} alt=""/>                           
-                        </a>
-                    </div>
-                    <div className='socialMedia_icon_container'>
-                        <a className='Footer_socialmedia_link' href='https://www.instagram.com/spaycserviciospersonasmayores/' target="_blank" rel="noopener noreferrer">
-                            <img className='socialMedia_icon' src={instagram_circle_black_icon} alt=""/>                           
-                        </a>
-                    </div>
                 </div>
             </div>
             <div className='Footer_secondSection_container'>
                 <ul className='Footer_secondSection_ul'>
                     <li><p>Contactanos</p></li>
-                    <li><a href='tel:3515459115' target="_blank" rel="noopener noreferrer">351 545 9115</a></li>
+                    <li><a href='https://wa.me/5493547571781?text=SPAYC%20leí%20los%20servicios%20en%20su%20página%20y%20quería%20consultar%20acerda%20de:' target="_blank" rel="noopener noreferrer">3547 571781</a></li>
                     <li><a href='mailto:spaycserviciosprofesionales@gmail.com' target="_blank" rel="noopener noreferrer">spaycserviciosprofesionales@gmail.com</a></li>
-                    <li>dirección</li>
                 </ul>
+                <div className='Footer_secondSection_socialMedia'> 
+                    <div className='socialMedia_container'>
+                        <div className='socialMedia_icon_container'>
+                            <a className='Footer_socialmedia_link' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                                <img className='socialMedia_icon' src={facebook_circle_black_icon} alt="" />
+                            </a>
+                        </div>
+                        <div className='socialMedia_icon_container'>
+                            <a className='Footer_socialmedia_link' href='https://www.instagram.com/spaycserviciospersonasmayores/' target="_blank" rel="noopener noreferrer">
+                                <img className='socialMedia_icon' src={instagram_black_icon} alt=""/>                           
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
